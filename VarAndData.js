04595f76a9ff41eaeb3e2c93a8777f70bf1351ff
WaqarDev69 <waqardev69@gmail.com>
2024-08-04 16:20:07 +0500

    first commit

---
+++ VarAndData.js
@@ -0,0 +1,54 @@
+var num = 69;
+console.log(typeof (num));
+
+let str = 'waqar';
+console.log(typeof (str));
+
+const boolean = true;
+console.log(typeof (boolean));
+
+let arr = [1, 2, 34]
+console.log(typeof (arr));
+
+let obj = {
+    age: 99,
+    gender: 'male'
+}
+console.log(typeof (obj));
+
+
+// Reassigning value 
+let age = 12;
+console.log(age);
+age = 23;
+console.log(age);
+
+const fname = 'waqar';
+console.log(fname);
+// fname = 'waqar khan'
+// console.log(fname); //error you can not reassign the const value
+
+
+
+let number = 12
+let string = 'hello'
+let booleans = true;
+let array = [1, 2, true, null];
+let nul = null;
+let bigint = 1234567867890n;
+
+console.log("value is", number, "type is", typeof (number));
+console.log("value is", string, "type is", typeof (string));
+console.log("value is", booleans, "type is", typeof (booleans));
+console.log("value is", array, "type is", typeof (array));
+console.log("value is", nul, "type is", typeof (nul));
+console.log("value is", bigint, "type is", typeof (bigint));
+
+
+
+
+
+
+
+
+
