7d0e19129dff15c2979d32a6a2fbd37e63ef49c7
WaqarDev69 <waqar.dev69@gmail.com>
2024-08-24 17:30:16 +0500

    solved loop question

--- Loops.js
+++ Loops.js
@@ -1,23 +1,49 @@
 for (let i = 0; i <= 10; i++) {
-    console.log(i);
+    // console.log(i);
 }
 
-
-// Print Table Using For Loop
-for (let i = 1; i <= 10; i++) {
-    console.log(`5 x ${i} =   `, 5 * i);
-}
-
-// While lop 
+//calculate sum of all numbers
 let sum = 0;
-let counter = 1;
-while (counter <= 10) {
-    sum += counter
-    counter++
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+//console.log(sum);
+
+
+//print even numbers
+for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+        // console.log(i);
+    }
 }
 
-console.log("the sum of value is ", sum);
+let arr = ['waqar', true, 33]
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+}
 
 
+// Reversed the array using for loop 
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let reverseArray = []
+for (let i = array.length - 1; i >= 0; i--) {
+    reverseArray.push(array[i]);
+
+}
+// console.log(reverseArray);
+
+
+
+let str = "example sentence";
+let charToCount = 'e';
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === charToCount) {
+        count++; // Increment counter
+    }
+}
+
+console.log(count);
 
 
